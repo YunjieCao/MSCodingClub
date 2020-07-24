315883f849f9a9f67a157ad7ba199866cf863cdb
Yunjie Cao <t-yuncao@microsoft.com>
2020-07-24 16:57:20 +0800

    fix bugs in 1178

--- week31_1178.java
+++ week31_1178.java
@@ -1,24 +1,19 @@
+import java.util.*;
 public class week31_1178 {
     class Solution {
         public List<Integer> findNumOfValidWords(String[] words, String[] puzzles) {
             List<Integer> ret = new ArrayList<>();
             int n = words.length;
-            HashMap<Character, List<Long>> map = new HashMap<>();
+            long[] words_ = new long[n];
             for (int i = 0; i < n; i++) {
-                long cur = getNum(words[i]);
-                List<Long> temp = map.getOrDefault(words[i].charAt(0), new ArrayList<>());
-                temp.add(cur);
-                map.put(words[i].charAt(0), temp);
+                words_[i] = getNum(words[i]);
             }
             for (String puzzle: puzzles) {
                 long cur = getNum(puzzle);
-                char first = puzzle.charAt(0);
+                long first = getNum(puzzle.substring(0,1));
                 int temp = 0;
-                if (!map.containsKey(first)) {
-                    ret.add(0);
-                    continue;
-                }
-                for (Long w: map.get(first)) {
+                
+                for (long w: words_) {
                     if ((w&first) == 0) continue;
                     long target = (w & cur);
                     if ((target^w) == 0) {
